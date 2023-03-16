19366f31cbf70f537df4021f3a0163cb45823280
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-03-16 15:36:15 +0300

    Progress

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -1,18 +1,52 @@
 class CakesController < ApplicationController
     wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
     def index
-        cakes = Cake.all
-        render json: cakes
+        cake = Cake.all
+        render json: cake
     end
 
     def create
-        cakes = Cake.create(cake_params)
-        render json: cakes, status: :created
+        cake = Cake.create(cake_params)
+        render json: cake, status: :created
     end
 
+    def show
+        cake = find_cake
+        render json: cake
+    end
+
+    def update
+        cake = find_cake
+        if cake
+            cake.update(cake_params)
+            render json: cake
+        else
+            render_not_found_response
+        end
+    end
+
+    def destroy
+        cake = find_cake
+        if cake
+            cake.destroy
+            head :no_content
+        else
+            render_not_found_response
+        end
+    end
     private
 
     def cake_params
         params.permit(:name, :weight, :flavor, :ingredients)
+    end
+
+    def render_not_found_response
+        render json: {error: "Cake not found"}, status: :not_found
+    end
+
+    def find_cake
+        Cake.find_by(id: params[:id])
     end
 end
